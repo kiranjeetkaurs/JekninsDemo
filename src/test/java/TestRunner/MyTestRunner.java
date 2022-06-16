6f8a1531669b9f66c2321bdec48fb1b3730cf47d
kiranjeetkaurs <kiranjeetkaurs@gmail.com>
2022-06-16 13:01:48 -0500

    updated test runner feature file path

--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -5,8 +5,9 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 	@RunWith(Cucumber.class) 
-	@CucumberOptions(features =".\\src/test/resources/com/features",
+	@CucumberOptions(features =".\\/JekninsDemo/src/test/resources/com/features/",
 	glue= {"stepdefinitions","AppHooks"},
 	plugin={"pretty","html:target/HtmlReports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true,
